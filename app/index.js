7f8f8f401c6d3055b740de0023f57ede19fe11f3
LaanAnla <95160853+LaanAnla@users.noreply.github.com>
2023-08-28 00:09:23 +0200

    remove normalizeScroll and add postcss-100vh-fix

--- app/index.js
+++ app/index.js
@@ -17,7 +17,6 @@ class App {
 
     this.scrollSmoother = ScrollSmoother.create({
       effects: true,           // looks for data-speed and data-lag attributes on elements
-      normalizeScroll: true, 
       smooth: 2,               // how long (in seconds) it takes to "catch up" to the native scroll position
       smoothTouch: true,
       });
