e0680c42177b39da46582d297faa054b26c39ea2
LaanAnla <95160853+LaanAnla@users.noreply.github.com>
2023-08-28 15:59:13 +0200

    normalize true

--- app/index.js
+++ app/index.js
@@ -20,6 +20,7 @@ class App {
       smooth: 2,               // how long (in seconds) it takes to "catch up" to the native scroll position
       smoothTouch: true,
       ignoreMobileResize: true,
+      normalizeScroll: true,
       });
       this.createContent();
       this.createPreloader();
