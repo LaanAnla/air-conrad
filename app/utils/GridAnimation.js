9f67ca17888f1226a2470e2918d08e0b615c29f0
LaanAnla <95160853+LaanAnla@users.noreply.github.com>
2023-08-28 03:14:50 +0200

    gestion grid animation

--- app/utils/GridAnimation.js
+++ app/utils/GridAnimation.js
@@ -12,13 +12,15 @@ export default class GridAnimation {
   gridAnimation() {
 
     let mm = gsap.matchMedia()
+    let mm2 = gsap.matchMedia()
+
     mm.add("(min-width: 1100px)", () => {
 
       let gridTl = gsap.timeline({
         scrollTrigger: {
           trigger: ".grid-section",
           scrub: 1,
-          start: "top center",
+          start: "top center+=20%",
           end: "center+=20% bottom",
         },
         defaults: {
@@ -45,36 +47,37 @@ export default class GridAnimation {
       }, "start");
     })
 
-    let gridTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".grid-section",
-        scrub: 1,
-        start: "top center",
-        end: "bottom+=500 bottom",
-        markers: true,
-      },
-      defaults: {
-        ease: "power4.inOut"
-      }
-    });
-    
-    gridTl.add("start")
-      .from(".grid-layout", {
-        ease: "power1",
-        scale: 3
-      }, "start")
-      .from(".column-1 .grid-image", {
-        duration: 1.2,
-        autoAlpha: 0,
-        xPercent: i => -((i + 1) * 40 + i * 100),
-        yPercent: i => (i + 1) * 40 + i * 100,
-      }, "start")
-      .from(".column-3 .grid-image", {
-        duration: 1.2,
-        autoAlpha: 0,
-        xPercent: i => (i + 1) * 40 + i * 100,
-        yPercent: i => (i + 1) * 40 + i * 100
-      }, "start");
+    mm2.add("(max-width: 1099px)", ()=> {
+      let gridTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".grid-section",
+          scrub: 1,
+          start: "top center",
+          end: "bottom+=500 bottom",
+        },
+        defaults: {
+          ease: "power4.inOut"
+        }
+      });
+      
+      gridTl.add("start")
+        .from(".grid-layout", {
+          ease: "power1",
+          scale: 3
+        }, "start")
+        .from(".column-1 .grid-image", {
+          duration: 1,
+          autoAlpha: 0,
+          xPercent: i => -((i + 1) * 40 + i * 100),
+          yPercent: i => (i + 1) * 40 + i * 100,
+        }, "start")
+        .from(".column-3 .grid-image", {
+          duration: 1,
+          autoAlpha: 0,
+          xPercent: i => (i + 1) * 40 + i * 100,
+          yPercent: i => (i + 1) * 40 + i * 100
+        }, "start");
+    })
   }
 }
 
