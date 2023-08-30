08647c5c423a56b27979a0739d5f99a858cc8a3d
LaanAnla <95160853+LaanAnla@users.noreply.github.com>
2023-08-30 15:18:19 +0200

    first commit

--- app/index.js
+++ app/index.js
@@ -1,12 +1,1 @@
-import myImage from '/shared/images.png'
-import myImage2 from '/shared/tree.jpg'
-
-const img = new Image();
-const img2 = new Image()
-img.src = myImage;
-img2.src = myImage2
-document.body.appendChild(img, img2);
-console.log('test de configurartion et d essai de clean')
-console.log('test de configurartion et d essai de clean')
-console.log('test de configurartion et d essai de clean')
-console.log('test de configurartion et d essai de clean')
+console.log('bienvenue')
